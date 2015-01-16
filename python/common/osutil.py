92fdeb36ef1b834b51018f0754ce948621922b4f
Cysu <st.cysu@gmail.com>
2015-01-16 11:11:55 +0800

    Ensure existance of a directory

--- python/common/osutil.py
+++ python/common/osutil.py
@@ -6,3 +6,8 @@ def check_files(root, file_name_list):
         file_path = os.path.join(root, file_name)
         if not os.path.isfile(file_path):
             raise ValueError("File not exist: " + file_path)
+
+def ensure_dir(path):
+    directory = os.path.dirname(path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
