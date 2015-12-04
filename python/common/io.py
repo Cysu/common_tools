227ea40e6d6d4c3b9d3343e415bd6eed12909dea
Cysu <st.cysu@gmail.com>
2015-12-04 11:36:43 +0800

    Change the interfaces of read_kv and write_kv

--- python/common/io.py
+++ python/common/io.py
@@ -25,6 +25,7 @@ def read_list(file_path, coding=None):
 
 def write_list(arr, file_path, coding=None):
     if coding is None:
+        arr = ['{}'.format(item) for item in arr]
         with open(file_path, 'w') as f:
             f.write('\n'.join(arr))
     else:
@@ -35,25 +36,51 @@ def write_list(arr, file_path, coding=None):
 def read_kv(file_path, coding=None):
     if coding is None:
         with open(file_path, 'r') as f:
-            dic = {}
+            key_list = []
+            value_list = []
             for line in f.readlines():
                 k, v = line.strip().split(' ', 1)
-                dic[k] = v
+                key_list.append(k)
+                value_list.append(v)
     else:
         with codecs.open(file_path, 'r', coding) as f:
-            dic = {}
+            key_list = []
+            value_list = []
             for line in f.readlines():
                 k, v = line.strip().split(' ', 1)
-                dic[k] = v
-    return dic
+                key_list.append(k)
+                value_list.append(v)
+    return key_list, value_list
 
 
-def write_kv(dic, file_path, coding=None):
+def write_kv(inp, file_path, coding=None):
     if coding is None:
         with open(file_path, 'w') as f:
-            for k, v in dic.iteritems():
-                f.write(k + ' ' + v + '\n')
+            if isinstance(inp, dict):
+                for k, v in inp.iteritems():
+                    f.write('{} {}\n'.format(k, v))
+            elif isinstance(inp, tuple) and len(inp) == 2:
+                for k, v in zip(*inp):
+                    f.write('{} {}\n'.format(k, v))
+            elif isinstance(inp, list):
+                for k, v in inp:
+                    f.write('{} {}\n'.format(k, v))
+            else:
+                raise ValueError("Invalid input data type. Should be a "
+                                 "dict or a tuple of (key_list, value_list) or "
+                                 "a list of [(key, value), ...]")
     else:
         with codecs.open(file_path, 'w', coding) as f:
-            for k, v in dic.iteritems():
-                f.write(k + u' ' + v + u'\n')
+            if isinstance(inp, dict):
+                for k, v in inp.iteritems():
+                    f.write(u'{} {}\n'.format(k, v))
+            elif isinstance(inp, tuple) and len(inp) == 2:
+                for k, v in zip(*inp):
+                    f.write(u'{} {}\n'.format(k, v))
+            elif isinstance(inp, list):
+                for k, v in inp:
+                    f.write(u'{} {}\n'.format(k, v))
+            else:
+                raise ValueError("Invalid input data type. Should be a "
+                                 "dict or a tuple of (key_list, value_list) or "
+                                 "a list of [(key, value), ...]")
